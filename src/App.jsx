024df6d744fab4781c093284cbde537aa140230d
akashsingh_bhandari <asbhandari14@gmail.com>
2024-04-19 07:04:56 +0530

    Install vite and step up tailwind and react router dom in our ecommerce project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+
+const App=()=>{
+
+  return(
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
