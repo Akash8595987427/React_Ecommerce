dd47d9846eefe049105f4fdf80f62fca7f0b3977
akashsingh_bhandari <asbhandari14@gmail.com>
2024-04-19 20:25:58 +0530

    Calling api and fetch api data using useEffect and async await

--- src/Context/product_context.jsx
+++ src/Context/product_context.jsx
@@ -1,11 +1,23 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect, useState } from "react"
 
 const AppContext = React.createContext();
 
 
 const AppProvider=({children})=>{
+    // const [data, setData] = useState([]);
+
+    const getApiData=async()=>{
+        const api_data = await fetch("https://api.pujakaitem.com/api/products");
+        const data = await api_data.json();
+        console.log(data);
+    }
+
+    useEffect(()=>{
+        getApiData();
+    }, [])
+
     return(
-        <AppContext.Provider value={{name : "Akash", age : 21}}>
+        <AppContext.Provider value={{name : "Akash Singh Bhandari"}}>
             {children}
         </AppContext.Provider>
     )
