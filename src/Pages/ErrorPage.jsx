84dba3a9fbbf19307db20c4984e4aa57de2e7ec0
akashsingh_bhandari <asbhandari14@gmail.com>
2024-04-19 19:56:23 +0530

    Setup the product_context api using the useContext hook and show context api value in the Error Page

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useGlobalContext } from '../Context/product_context'
 
 
 const ErrorPage = () => {
+  const {name} = useGlobalContext();
   return (
     <div className='w-full h-[60vh] flex flex-col justify-center items-center gap-3 my-8'>
         <h1 className='text-5xl font-semibold'>404</h1>
         <p className='uppercase text-xl'> Uh Oh ! You're lost </p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt, et.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt, et. {name} </p>
         <button className='py-1.5 px-3 bg-purple-700 text-white'>  <NavLink to="/"> Home  </NavLink>  </button>
+        
     </div>
   )
 }
