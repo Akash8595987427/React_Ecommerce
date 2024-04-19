1edbe6b621e16ce8cbb5dc8421a665d09a98a5dd
akashsingh_bhandari <asbhandari14@gmail.com>
2024-04-19 19:27:46 +0530

    Updating and Completing the Error404 Page

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>Hello I am the Error Page</h1>
+    <div className='w-full h-[60vh] flex flex-col justify-center items-center gap-3 my-8'>
+        <h1 className='text-5xl font-semibold'>404</h1>
+        <p className='uppercase text-xl'> Uh Oh ! You're lost </p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt, et.</p>
+        <button className='py-1.5 px-3 bg-purple-700 text-white'>  <NavLink to="/"> Home  </NavLink>  </button>
     </div>
   )
 }
